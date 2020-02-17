b9dd0b639942a462a6173880ffb64bc21f11490c
Chethan <Chethan@192.168.1.2>
2020-02-17 12:14:06 +0530

    1st Commit:
    Creating Java files for respective Problems.

---
+++ com.ScalarOnlineTestAlgorithms/src/com/ScalarOnlineTestAlgorithms/SortedSubsequences.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.ScalarOnlineTestAlgorithms;
+
+/**
+ * @author Chethan
+ *
+ */
+public class SortedSubsequences {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
